a8aa962cf54867c4d261694f46a03cbe65ad5da3
NefytZ <thyphen45@live.fr>
2022-11-29 10:41:14 +0100

    route series

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,13 +6,22 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
+#[Route('/program', name: 'program_')] 
 class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
+    #[Route('/', name: 'program_index')]
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Series',
          ]);
     }
+    #[Route('/show/{id}', methods: ['GET'], requirements: ['id'=>'\d+'], name: 'program_show')]
+    public function show(int $id): Response
+        {
+            return $this->render('program/show.html.twig' , 
+            ['id' => $id]);
+        }
+   
+    
 }
