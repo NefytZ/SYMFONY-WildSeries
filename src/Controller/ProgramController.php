0b29fd8d7876a5008f88e638b6f5330d98de4873
NefytZ <thyphen45@live.fr>
2022-12-01 11:40:52 +0100

    10 - Symfony : Récupérer des données stockées avec Doctrine

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,26 +2,36 @@
 // src/Controller/ProgramController.php
 namespace App\Controller;
 
+use App\Repository\ProgramRepository;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 
-#[Route('/program', name: 'program_')] 
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
-    #[Route('/', name: 'program_index')]
-    public function index(): Response
+    #[Route('/', name: 'index')]
+    public function index(ProgramRepository $programRepository): Response
     {
+        $programs = $programRepository->findAll();
         return $this->render('program/index.html.twig', [
-            'website' => 'Wild Series',
-         ]);
+                'programs' => $programs,
+            ]);
     }
-    #[Route('/show/{id}', methods: ['GET'], requirements: ['id'=>'\d+'], name: 'program_show')]
-    public function show(int $id): Response
-        {
-            return $this->render('program/show.html.twig' , 
-            ['id' => $id]);
+    #[Route('/show/{id}', name: 'show')]
+    public function show(int $id, ProgramRepository $programRepository): Response
+    {
+        $program = $programRepository->findOneBy(['id' => $id]);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No Program with id : ' . $id . ' found'
+            );
         }
-   
-    
+
+        return $this->render(
+            'program/show.html.twig',
+            ['program' => $program]
+        );
+    }
 }
